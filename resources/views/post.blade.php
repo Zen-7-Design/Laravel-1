78c7cd6075368fd4bc6b6803679642bde2adb754
Zen-7-Design <azkiputraanimers2@gmail.com>
2022-11-09 08:49:49 +0700

    Update 9 November 2022

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,8 +8,9 @@
 <h2>
     {{ $post->title }}
 </h2>
-<h5>{{ $post->author }}</h5>
-<p>{{ $post->body }}</p>
+<h5>{{ $post->author }}</h5> 
+
+{!! $post->body !!}
 </article>
 
 <a href="/blog">Back To Posts</a>
